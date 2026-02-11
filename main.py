5ef6198e59b04648226477dd89bf9a2367a6f3bb
root <root@ShlokDesktop.localdomain>
2026-02-11 03:12:09 +0000

    slightly modified main and added logging state checker

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+from constans import SCREEN_HEIGHT,SCREEN_WIDTH
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_HEIGHT}")
+    print(f"Screen height: {SCREEN_WIDTH}")
+
+
+if __name__ == "__main__":
+    main()
