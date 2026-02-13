f06fefd70ecc670b4d58b7746fd7c40b1f497569
root <root@ShlokDesktop.localdomain>
2026-02-13 05:25:34 +0000

    player movement and asteroids added

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame
 from constants import SCREEN_HEIGHT,SCREEN_WIDTH
 from logger import log_state
 from player import Player
+from asteroids import Asteroid
+from asteroidfield import AsteroidField
+
 def main():
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,16 +12,25 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable,drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
     player_one = Player(SCREEN_WIDTH/2 , SCREEN_HEIGHT/2)
     
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
                 return
+        
+        updatable.update(dt)
         screen.fill("black")
-        player_one.draw(screen)
+        for drawables in drawable:
+            drawables.draw(screen)
         pygame.display.flip()
         dt = (clock.tick(60))/1000
 
